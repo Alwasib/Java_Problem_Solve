f0fa1b5c6ae5d899cc6818cd322729a1d5dcd3a1
alwasib <122206303+Alwasib@users.noreply.github.com>
2024-09-02 22:39:49 +0600

    Add files via upload

---
+++ DataType.java
@@ -0,0 +1,30 @@
+public class DataType {
+    public static void main(String [] args)
+    {
+        boolean b= true;
+        byte bt= 127;
+        int i=10;
+        char c='M';
+        short s=32564;
+        float f= 10.5f;
+        double d=12.5;
+        System.out.println("b = "+b);
+        System.out.println("bt = "+bt);
+        System.out.println("i = "+i);
+        System.out.println("c = "+c);
+        System.out.println("s = "+s);
+        System.out.println("f = "+f);
+        System.out.println("d = "+d);
+        int id=101;
+        String title="Iphone15";
+        String price="1500 US$";
+        String description="Wonderful image quality";
+        String category="phone";
+        System.out.println("id = "+id);
+        System.out.println("title = "+title);
+        System.out.println("price = "+price);
+        System.out.println("description = "+description);
+        System.out.println("category = "+category);
+    }
+
+}
